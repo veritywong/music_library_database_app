21634d9fafa7381164f9d35a2153f3d2501907cb
Verity <veritywong90@gmail.com>
2023-05-05 11:41:51 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,113 @@
+# file: app.rb
+require 'sinatra/base'
+require 'sinatra/reloader'
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+
+  get '/' do
+    return erb(:index)
+  end
+
+  get '/about' do
+    return erb(:about)
+  end
+
+  get '/albums' do
+    repo = AlbumRepository.new
+    @albums = repo.all
+
+    return erb(:albums)
+
+    # first solution:
+    # response = albums.map do |album|
+    #   album.title
+    # end.join(', ')
+
+    # return response
+
+    # solution to return title and release year
+    # response = albums.map do |album|
+    #   "Title: #{album.title}, Release year: #{album.release_year}"
+      
+    # end.join(', ')
+    # return response
+    
+  end
+
+  get '/albums/new' do
+    return erb(:album_new)
+  end
+
+  get '/albums/:id' do
+    id = params[:id]
+
+    repo = AlbumRepository.new
+    artist_repo = ArtistRepository.new
+
+    @album = repo.find(id)
+    # @album = repo.find(params[:id])
+    @artist = artist_repo.find(@album.artist_id)
+
+    return erb(:album_id)
+  end
+
+  post '/albums' do
+    repo = AlbumRepository.new
+    new_album = Album.new
+
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+
+    repo.create(new_album)
+
+    return ''
+  end
+
+  get '/artists' do
+    repo = ArtistRepository.new
+    @artists = repo.all
+
+    return erb(:artists)
+
+    # response = artists.map do |artist|
+    #   artist.name
+    # end.join(', ')
+    # return response
+  end
+
+  get '/artists/new' do
+    return erb(:artist_new)
+  end
+
+  get '/artists/:id' do
+    id = params[:id]
+    repo = ArtistRepository.new
+    @artist = repo.find(id)
+
+    return erb(:artist_id)
+  end
+
+  post '/artists' do
+    repo = ArtistRepository.new
+    new_artist = Artist.new
+
+    new_artist.name = params[:name]
+    new_artist.genre = params[:genre]
+  
+    repo.create(new_artist)
+
+    return ''
+  end
+
+end
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,135 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+  def reset_albums_table
+    seed_sql = File.read('spec/seeds/albums_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+  
+  describe AlbumRepository do
+    before(:each) do 
+      reset_albums_table
+    end
+
+    context 'GET /albums' do
+      it 'should return the list of albums' do
+        response = get('/albums')
+        
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<a href="albums/1">Doolittle</a><br />')
+        expect(response.body).to include('<a href="albums/2">Surfer Rosa</a><br />')
+
+
+      end 
+    end
+
+    context 'GET /albums/:id' do
+      it 'should return the content for album 1' do
+        response = get('/albums/1')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<h1>Doolittle</h1>')
+        expect(response.body).to include('Release year: 1989')
+        expect(response.body).to include('Artist: Pixies')
+      end
+    end
+
+    context 'GET /albums/new' do
+      it 'should return form' do
+        response = get('/albums/new')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<form action="/albums" method="POST">')
+        expect(response.body).to include('<input type="text" name="title">')
+        expect(response.body).to include('<input type="text" name="release_year">')
+        expect(response.body).to include('<input type="text" name="artist_id">')
+        expect(response.body).to include('<input type="submit" value="Submit album">')
+      end
+    end
+
+    context 'POST /albums' do
+      it 'should create a new albums' do
+        response = post('/albums', title: 'OK Computer', release_year: '1997', artist_id: '1')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        # response = get('/albums')
+        # expect(response.body).to include('OK Computer')
+      end
+    end
+
+ end
+
+  def reset_artists_table
+    seed_sql = File.read('spec/seeds/artists_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  describe ArtistRepository do
+    before(:each) do 
+      reset_artists_table
+    end
+
+    context 'GET /artists' do
+      it 'returns a list of artists' do
+        response = get('/artists')
+        
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<a href="artists/1">Pixies</a><br />')
+      end
+    end
+
+    context 'GET /artists/new' do
+      it 'returns an html form' do
+        response = get('/artists/new')
+
+        expect(response.status).to eq 200
+        expect(response.body).to include '<form action="/artists" method="POST">'
+        expect(response.body).to include '<input type="text" name="name">'
+        expect(response.body).to include '<input type="text" name="genre">'
+        expect(response.body).to include '<input type="submit" value="Submit artist">'
+      end
+    end
+
+    context 'GET /artists/:id' do
+      it 'returns artist 1' do
+        response = get('/artists/1')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<h1>Pixies</h1>')
+      end
+    end
+
+
+    context 'POST /artists' do
+      it 'should create a new artist' do
+        response = post('/artists', name: 'Beyonce', genre: 'Pop')
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        response = get('/artists')
+        expect(response.body).to include('Beyonce')
+      end
+    end
+
+  end
+end
+
+
+# In the project music_library_database_app.
+
+# Test-drive and implement a form page to add a new album.
+
+# You should then be able to use the form in your web browser to add a new album, and see this new album in the albums list page.
